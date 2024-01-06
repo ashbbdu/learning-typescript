92a622f3d0411ade389264d4017e5c248e9b4f94
Ashish Srivastava <ashishsrivastava@Ashishs-MacBook-Pro.local>
2024-01-06 18:35:06 +0530

    add todo api intergration done

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,65 +1,65 @@
 export interface Person {
     firstName: string;
     lastName: string;
-  }
+}
 
-  interface Rating {
-    count : number,
-    rate : number
+interface Rating {
+    count: number,
+    rate: number
 }
 
 export interface MyObj {
-    category : string,
-    description : string,
-    id : number,
-    image : string,
-    price : number,
-    rating : Rating,
-    title : string
+    category: string,
+    description: string,
+    id: number,
+    image: string,
+    price: number,
+    rating: Rating,
+    title: string
 }
 
 export interface Data {
-    firstName : string,
-    lastName : string,
-    age : number
+    firstName: string,
+    lastName: string,
+    age: number
 }
 
 export interface CardProps {
-    title : string,
-    description : string
+    title: string,
+    description: string
 }
 
 export interface CreateTodo {
-    title : string,
-    description : string
+    title: string,
+    description: string
 }
 
 export interface Todos {
-    _id : number,
-    title : string,
-    description : string,
-    completed : boolean,
-  }
+    _id: number,
+    title: string,
+    description: string,
+    completed: boolean,
+}
 
 export const data = [
     {
-        id : 1,
-        title : "First Card",
-        description : "Hi There, This is the First Card"
+        id: 1,
+        title: "First Card",
+        description: "Hi There, This is the First Card"
     },
     {
-        id : 2,
-        title : "Second Card",
-        description : "Hi There, This is the Second Card"
+        id: 2,
+        title: "Second Card",
+        description: "Hi There, This is the Second Card"
     },
     {
-        id : 3,
-        title : "Third Card",
-        description : "Hi There, This is the Third Card"
+        id: 3,
+        title: "Third Card",
+        description: "Hi There, This is the Third Card"
     },
     {
-        id : 4,
-        title : "Fourth Card",
-        description : "Hi There, This is the Fourth Card"
+        id: 4,
+        title: "Fourth Card",
+        description: "Hi There, This is the Fourth Card"
     }
 ]
